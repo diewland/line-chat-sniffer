c4664d68bcc645cf0f9c8e1f3ef6151c9299bc48
diewland <diewland@gmail.com>
2019-08-12 16:04:19 +0700

    revise sniff

--- sniffer.py
+++ sniffer.py
@@ -1,44 +1,35 @@
+import os
 from flask import Flask, request, abort
-from linebot import ( LineBotApi, WebhookHandler)
-from linebot.exceptions import ( InvalidSignatureError )
-from linebot.models import ( MessageEvent, TextMessage, TextSendMessage )
-
-ACCESS_TOKEN = '' # TODO change to channel access token
-SECRET = ''       # TODO change to channel secret
+from pprint import pprint as pp
 
 app = Flask(__name__)
 
-line_bot_api = LineBotApi(ACCESS_TOKEN)
-handler = WebhookHandler(SECRET)
+def print_text(tag, resp):
+    if len(resp['events']) > 0:
+        this_evt = resp['events'][0]
+        this_msg = this_evt.get('message', {})
+        this_src = this_evt.get('source', {})
+
+        # filter only text by user
+        if (this_msg.get('type') == 'text') and this_src.get('type') == 'user' :
+            info = {
+                'user_id': this_src.get('userId'),
+                'text': this_msg.get('text'),
+                'timestamp': this_evt.get('timestamp'),
+                'destination': resp.get('destination'),
+            }
+            print(tag, info)
 
 @app.route("/callback", methods=['POST'])
 def callback():
-    # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
 
-    # get request body as text
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
+    tag = '<1337>'
+    resp = request.json
 
-    # handle webhook body
-    try:
-        handler.handle(body, signature)
-    except InvalidSignatureError:
-        print("Invalid signature. Please check your channel access token/channel secret.")
-        abort(400)
+    print(tag, resp)
+    #print_text(tag, resp)
 
     return 'OK'
-
-@handler.add(MessageEvent, message=TextMessage)
-def handle_message(event):
-    #
-    # write your logic here
-    #
-    # MessageEvent - https://github.com/line/line-bot-sdk-python/blob/master/linebot/models/events.py
-    # SourceUser   - https://github.com/line/line-bot-sdk-python/blob/master/linebot/models/sources.py
-    # TextMessage  - https://github.com/line/line-bot-sdk-python/blob/master/linebot/models/messages.py
-    #
-    print("[%s] %s" % (event.source.user_id, event.message.text))
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True, threaded=True)
